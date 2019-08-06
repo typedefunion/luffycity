7d50e7623e7a116579d4316e7656a037e832d55a
wangsijie <2245713938@qq.com>
2019-08-06 22:21:36 +0800

    新建前端项目luffy_pc,导入路由并设置路由

---
+++ luffy_pc/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="home">
+    前端首页
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ luffy_pc/src/routers/index.js
@@ -0,0 +1,25 @@
+import Vue from "vue"
+import Router from "vue-router"
+
+// 这里导入可以让让用户访问的组件
+import Home from '../components/Home'
+
+Vue.use(Router);
+
+export default new Router({
+  // 设置路由模式为‘history’，去掉默认的#
+  mode: "history",
+  routes:[
+    // 路由列表
+      {
+       name:"Home",
+       path:"/",
+       component:Home,
+     },
+      {
+       name:"Home",
+       path:"/home",
+       component:Home,
+     },
+  ]
+})
