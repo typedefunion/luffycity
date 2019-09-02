31f894a3d27733002a143d476939d7680738bf6e
wangsijie <45301517+typedefunion@users.noreply.github.com>
2019-09-02 19:15:33 +0800

    Update prod.py

--- luffyapi/luffyapi/settings/prod.py
+++ luffyapi/luffyapi/settings/prod.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import sys
-
+# 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # 让spps设置为系统导包路径
